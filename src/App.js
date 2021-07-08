3ca0f5c66822a8da3c5dd9d4d8102cbbb2a1f335
Richard G. Roman <rits@rits.hu>
2021-07-08 15:06:32 +0200

    Section83 - Header Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 import { Route, Switch } from "react-router-dom";
 
 const HatsPage = () => (
@@ -13,10 +14,11 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-      <Route path="/shop/hats" component={HatsPage} />
-      <Route path="/shop" component={ShopPage} />
-      <Route path="/" component={HomePage} />
+        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/" component={HomePage} />
       </Switch>
     </div>
   );
