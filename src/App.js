dd94d46bcc8e5a755f0dc88f498f4fb105fce93c
Richard G. Roman <rits@rits.hu>
2021-07-08 14:45:51 +0200

    Section82 - CollectionItem

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import { Route, Switch } from "react-router-dom";
 
 const HatsPage = () => (
@@ -14,6 +15,7 @@ function App() {
     <div>
       <Switch>
       <Route path="/shop/hats" component={HatsPage} />
+      <Route path="/shop" component={ShopPage} />
       <Route path="/" component={HomePage} />
       </Switch>
     </div>
